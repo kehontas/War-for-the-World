e9f3d75b59d494e2d3eaf7a47cc3ebf8bc1afc93
kehontas <kehontas@gmail.com>
2015-11-25 13:18:56 -0800

    trying to get keypress to work

--- game-1/main.js
+++ game-1/main.js
@@ -1,32 +1,28 @@
  $(function(){
  	console.log("Sanity Check");
  
+	if (keypress === 97) {
+		var ply1 = 0;
+		 $(".box0").keypress(function() {
+		     //$('.box1.start').text("o");
+		     $(".box0").eq(ply1).text("");
+		     ply1 ++;
+		     $(".box0").eq(ply1).text("x");
+		});	
+	} else if (keypress === 13) {
+	 	var ply2 = 0;
+	 	$(".box1").keypress(function() {
+	 	// $(window).click(function() {
+	 	     //$('.box1.start').text("o");
+	 	     $(".box1").eq(ply2).text("");
+	 	     ply2 ++;
+	 	     $(".box1").eq(ply2).text("o");
+	 	 });
+	 
+	}
 
-//  $('.box0.start').click(function(e) {
-//      // for each click add an x
-//      //  	box0 ++
-//       $('.box0.start').text("x");
-// });
+}); 
 
-  //if (key press = keyCode: 97 value)
-var ply1 = 0;
- $(window).click(function() {
-     //$('.box1.start').text("o");
-     $(".box0").eq(ply1).text("");
-     ply1 ++;
-     $(".box0").eq(ply1).text("x");
-});
-
-//else if (key press = 13; value)
-var ply2 = 0;
- $(window).click(function() {
-     //$('.box1.start').text("o");
-     $(".box1").eq(ply2).text("");
-     ply2 ++;
-     $(".box1").eq(ply2).text("o");
-});
-
-});
 
 
 //$('.start').click(function(e) {
