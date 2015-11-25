89140a8a3b79717bb0c2fa8d5f41b0168d83cf59
kehontas <kehontas@gmail.com>
2015-11-24 19:27:53 -0800

    my starting point, simple board with onClick working on player and player two start

---
+++ game-1/main.js
@@ -0,0 +1,20 @@
+ $(function(){
+ 	console.log("Sanity Check");
+ 
+
+ $('.box0.start').click(function(e) {
+      $('.box0.start').text("x");
+});
+
+ $('.box1.start').click(function(e) {
+      $('.box1.start').text("x");
+});
+
+});
+
+
+//$('.start').click(function(e) {
+//     $('.start').text("x");
+//});
+//puts x on first box
+ 
