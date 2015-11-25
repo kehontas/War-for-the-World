df72d6f21bd16d60933aa3a3bc007a83687039e0
kehontas <kehontas@gmail.com>
2015-11-25 12:44:59 -0800

    game pieces moving across the screen

--- game-1/main.js
+++ game-1/main.js
@@ -2,14 +2,28 @@
  	console.log("Sanity Check");
  
 
- $('.box0.start').click(function(e) {
-      for each click add an x
-      	box ++
-      $('.box0.start').text("x");
+//  $('.box0.start').click(function(e) {
+//      // for each click add an x
+//      //  	box0 ++
+//       $('.box0.start').text("x");
+// });
+
+  //if (key press = keyCode: 97 value)
+var ply1 = 0;
+ $(window).click(function() {
+     //$('.box1.start').text("o");
+     $(".box0").eq(ply1).text("");
+     ply1 ++;
+     $(".box0").eq(ply1).text("x");
 });
 
- $('.box1.start').click(function(e) {
-      $('.box1.start').text("x");
+//else if (key press = 13; value)
+var ply2 = 0;
+ $(window).click(function() {
+     //$('.box1.start').text("o");
+     $(".box1").eq(ply2).text("");
+     ply2 ++;
+     $(".box1").eq(ply2).text("o");
 });
 
 });
