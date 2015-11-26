0b0e41bd9a40160b15083dcf3f547e570b843dd0
kehontas <kehontas@gmail.com>
2015-11-26 13:54:38 -0800

    auto reset on win

--- game-1/main.js
+++ game-1/main.js
@@ -7,15 +7,21 @@
 		$(window).on("keypress", function handler(event) {
 			if (ply1 === 7 && ply2 === 7){
 				console.log("We are both here!");
+				location.reload();
+				
 				}
-			else if (event.which === 97) {
+				
+
+			 else if (event.which === 97) {
 				if (ply1 < 7) {
 					$(".box0").eq(ply1).text("");
 			    	ply1 ++;
 			    	$(".box0").eq(ply1).text("X");
-				} else if (ply1 === 7) {
+			    } else if (ply1 === 7) {
 					console.log("Im here");
+					location.reload();
 				}
+				
 			 } else if (event.which === 39) {
 				if (ply2 < 7) {
 					$(".box1").eq(ply2).text("");
@@ -23,18 +29,19 @@
 		 	    $(".box1").eq(ply2).text("O");
 				} else if (ply2 === 7){
 				console.log("Im here");
-				}	
-			// }else if (ply1 && ply2 === 7){
-			// 	console.log("We are both here!");
-			// 	}
-			}
-		$("button").click(function handleClick(event){
-			location.reload();
-
-		});
-	});
+				location.reload();
+				}
+				
+			
 		
-}); 
+}
+		});$("button").click(function handleClick(event){
+		location.reload();
+});
+		
+	});
+			
+
 
  // $("button").click(function handleClick(event){
 	// $("div.box").text("");
