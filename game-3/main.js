c99b0e806efae0e63fe181a32afa4c468d8a18b6
kehontas <kehontas@gmail.com>
2015-11-29 15:39:31 -0800

    Game 3, added 3,2,1 counter

--- game-3/main.js
+++ game-3/main.js
@@ -14,11 +14,24 @@ playerTwo.src = "http://cdn.mysitemyway.com/icons-watermarks/simple-black/bfa/bf
 var youLose = new Image();
 youLose.src = "http://efod.se/media/blog/explosion3.png";
 
-		
-		$(window).on("keypress", function handler(event) {
-			
-			
+		$(function(){
+			var timer = setInterval(function(){
+			$("#count_num").html(function(i,html){
+   
+			if(parseInt(html)>1)
+   	{
+   		return parseInt(html)-1;
+   	}
+   		else
+   {
+   	clearTimeout(timer);
+       return "GO!";
+   }
+});
+		},1000);
+});		
 
+		$(window).on("keypress", function handler(event) {
 			 if (event.which === 97) {
 				if (ply1 < 9) {
 					$(".box0").eq(ply1).text("");
@@ -29,8 +42,7 @@ youLose.src = "http://efod.se/media/blog/explosion3.png";
 					$(".box1").eq(ply2).text("").append(youLose);
 					$(window).off('keypress');
 					//location.reload();
-					$( "h3" ).html( "Player 1 is the Winner, Play Again!" );
-					
+					$( "h3" ).html( "Player 1 is the Winner, Play Again!" );	
 				}
 				
 			} else if (event.which === 108) {
@@ -44,9 +56,8 @@ youLose.src = "http://efod.se/media/blog/explosion3.png";
 
 					$(".box0").eq(ply1).text(" ").append(youLose);
 					$( "h3" ).html( "Player 2 is the Winner, Play Again!" );
-
 					
-}
+				}
 			 
 			if (ply1 === 9 && ply2 === 9){
 			console.log("We are both here!");
@@ -58,10 +69,10 @@ youLose.src = "http://efod.se/media/blog/explosion3.png";
 		}
 
 		});$("button").click(function handleClick(event){
-		location.reload();
-});
-		
+			location.reload();
 	});
+		
+});
 
  
 
