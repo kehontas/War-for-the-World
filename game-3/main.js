d8edfb02e076b579617f3ffd002fbcbdae8db181
kehontas <kehontas@gmail.com>
2015-11-29 16:28:20 -0800

    Game 3

--- game-3/main.js
+++ game-3/main.js
@@ -14,26 +14,22 @@ playerTwo.src = "http://cdn.mysitemyway.com/icons-watermarks/simple-black/bfa/bf
 var youLose = new Image();
 youLose.src = "http://efod.se/media/blog/explosion3.png";
 
-		$(function(){
-			var timer = setInterval(function(){
-			$("#count_num").html(function(i,html){
-   
-			if(parseInt(html)>1)
-   	{
-   		return parseInt(html)-1;
-   	}
-   		else
-   {
+$(function(){
+	var timer = setInterval(function(){
+	$("#count_num").html(function(i,html){
+  if(parseInt(html)>1){
+   	return parseInt(html)-1;
+  }else{
    	clearTimeout(timer);
        return "GO!";
-   }
+  }
 });
-		},1000);
+		},650);
 });		
 
 		$(window).on("keypress", function handler(event) {
 			 if (event.which === 97) {
-				if (ply1 < 9) {
+					if (ply1 < 9) {
 					$(".box0").eq(ply1).text("");
 			    	ply1 ++;
 			    	$(".box0").eq(ply1).append(playerOne);
@@ -41,37 +37,31 @@ youLose.src = "http://efod.se/media/blog/explosion3.png";
 					console.log("Im here");
 					$(".box1").eq(ply2).text("").append(youLose);
 					$(window).off('keypress');
-					//location.reload();
 					$( "h3" ).html( "Player 1 is the Winner, Play Again!" );	
 				}
 				
-			} else if (event.which === 108) {
-				if (ply2 < 9) {
+				}else if (event.which === 108) {
+					if (ply2 < 9) {
 					$(".box1").eq(ply2).text("");
 		 	    	ply2 ++;
 		 	    	$(".box1").eq(ply2).append(playerTwo);
-				} else if (ply2 >= 9){
+				}else if (ply2 >= 9){
 					console.log("Im here");
 					$(window).off('keypress');
 
 					$(".box0").eq(ply1).text(" ").append(youLose);
 					$( "h3" ).html( "Player 2 is the Winner, Play Again!" );
-					
 				}
 			 
-			if (ply1 === 9 && ply2 === 9){
-			console.log("We are both here!");
-			// location.reload();
+				if (ply1 === 9 && ply2 === 9){
+				console.log("We are both here!");
 				$( "h3" ).html( "It's a Tie, Play again!" );
 				$(window).off('keypress');
-			}
-			
+			}	
 		}
-
-		});$("button").click(function handleClick(event){
+			});$("button").click(function handleClick(event){
 			location.reload();
-	});
-		
+	});	
 });
 
  
